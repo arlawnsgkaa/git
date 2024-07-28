d511935bfc9ef12343dbbc9d5f45e97ce20d3378
arlawnsgka <arlawnsgkaa@naver.com>
2024-07-28 12:55:55 +0900

    fourthcommit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,14 +6,42 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /* 4. 반복문 사용 해서 연산을 반복 */
+        String answer = "y";
+        while (!answer.equals("exit")) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();// Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();// Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("사칙연산 기호를 입력하세요: ");
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char calculationSymbol = sc.nextLine().charAt(0);// 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            char calculationSymbol = sc.next().charAt(0);// 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+
+            int result = 0;
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            switch (calculationSymbol) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+
+            }
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            answer = sc.next();
+            /* exit을 입력 받으면 반복 종료 */
+        }
 
     }
 }
